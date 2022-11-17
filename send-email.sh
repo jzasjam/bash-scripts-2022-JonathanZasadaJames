546cd07391685ee1122faa96c507a017afa0f2e4
jzasjam <jonathan.zasada-james@student.shu.ac.uk>
2022-11-17 16:12:20 +0000

    Send emails added configuration/setup

--- send-email.sh
+++ send-email.sh
@@ -19,43 +19,56 @@ COLOR='\033[0m' # No Color
         echo "==================="
     fi
 
+    # Get From Email value from root=value in conf file
+    from_email="$(sudo cat /etc/ssmtp/ssmtp.conf | grep 'root=' | cut -d'=' -f2)"
+    # echo $from_email
+
 # Check if command is available
-    if [ ! command -v sendmail &> /dev/null ]
+    if [ ! command -v sendmail &> /dev/null ] || [ "$from_email" == "postmaster" ] || [ "$from_email" == null ]
     then
-        # If not available, install it 
-        warning="sendmail command could not be found"
+        warning="sendmail is not installed or root email is not set in ssmtp.conf"
         WARNING
+        #exit
+    
+        # If not available, install it 
+        #warning="sendmail command could not be found"
+        #WARNING
         echo "--------------------------"
-        echo "Install sendmail? (y/n)"
+        echo "Configure sendmail? (y/n)"
         read confirmation;
         if [ "$confirmation" = "y" ]
         then
             echo "y" | sudo apt-get install sendmail
 
             # UPDATE SMTP CONFIGURATION (/etc/ssmtp/ssmtp.conf)
-            : '
-            #
-            # Config file for sSMTP sendmail
-            #
-            # The person who gets all mail for userids < 1000
-            # Make this empty to disable rewriting.
-            root=c1051167@my.shu.ac.uk
+            
+            sudo cp ssmtp.conf.template /etc/ssmtp/ssmtp.conf
+            
+            # Ask for sending email address
+            echo "--------------------------"
+            echo "> Enter your sending email address:"
+            read email;
+            sudo sed -i "s/{root}/$email/g" /etc/ssmtp/ssmtp.conf
+            sudo sed -i "s/{hostname}/$email/g" /etc/ssmtp/ssmtp.conf
+            # Gett the domain name from the email address
+            domain="$(sudo cat /etc/ssmtp/ssmtp.conf | grep 'root=' | cut -d'@' -f2)"
+            sudo sed -i "s/{rewritedomain}/$domain/g" /etc/ssmtp/ssmtp.conf
 
-            # The place where the mail goes. The actual machine name is required no
-            # MX records are consulted. Commonly mailhosts are named mail.domain.com
-            mailhub=smtp.sendgrid.com:587
+            # Ask for SMTP Credentials
+            echo "--------------------------"
+            echo "> Enter your SMTP mail server (with port): (e.g. smtp.sendgrid.com:587)"
+            read mailhub;
+            sudo sed -i "s/{mailhub}/$mailhub/g" /etc/ssmtp/ssmtp.conf
+            
+            echo "--------------------------"
+            echo "> Enter your SMTP username: (eg. apikey)"
+            read authuser;
+            sudo sed -i "s/{authuser}/$authuser/g" /etc/ssmtp/ssmtp.conf
 
-            AuthUser=apikey
-            AuthPass={API-KEY-HERE}
-            UseTLS=YES
-            UseSTARTTLS=YES
-
-            # Where will the mail seem to come from?
-            rewriteDomain=my.shu.ac.uk
-
-            # The full hostname
-            hostname=c1051167@my.shu.ac.uk
-            '
+            echo "--------------------------"
+            echo "> Enter your SMTP password/API Key:"
+            read authpass;
+            sudo sed -i "s/{authpass}/$authpass/g" /etc/ssmtp/ssmtp.conf
 
         else
             echo "Sending Email Cancelled"
@@ -64,6 +77,11 @@ COLOR='\033[0m' # No Color
     fi
 
 # Send Email
+
+    # Get From Email value from root=value in conf file
+    from_email="$(sudo cat /etc/ssmtp/ssmtp.conf | grep 'root=' | cut -d'=' -f2)"
+    # echo $from_email
+
     echo -e "\nSend Email"
     echo -e "----------------------"
     echo "Send To Email >"
@@ -73,5 +91,5 @@ COLOR='\033[0m' # No Color
     echo "Message >"
     read message
     #echo $message | mail -s $subject $email
-    echo -e "Subject: $subject \nFrom:c1051167@my.shu.ac.uk \n\n $message" | sendmail $email
+    echo -e "Subject: $subject \nFrom:$from_email \n\n $message" | sendmail $email
     
